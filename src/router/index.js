1c184e80bbf51c0d08fada3ea26fea8a3dfdc670
Esther Morris <EstherMorris775@gmail.com>
2022-09-03 23:42:38 -0400

    MoviesCreate

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MoviesCreate from '../views/MoviesCreate.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/new',
+    component: MoviesCreate
   },
   {
     path: '/about',
---
+++ src/views/MoviesCreate.vue
@@ -0,0 +1,39 @@
+<script>
+  import { slotFlagsText } from "@vue/shared";
+import axios from "axios";
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        newMovie:{},
+        movies: []
+      };
+    },
+    created: function () {},
+    methods: {
+      movieCreate(){
+        console.log("creating a new movie...")
+        console.log(this.newMovie)
+        axios.post("http://localhost:3000/movies.json", this.newMovie).then(response => {
+          console.log(response.data)
+      this.movies.push(response.data)
+     })
+
+      }
+    },
+  };
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div>
+      Name: <input type="text" v-model="newMovie.name">
+      Year: <input type="text" v-model="newMovie.year">
+      <button v-on:click="movieCreate()"> Sumbit</button>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
